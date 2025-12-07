d6f8fe243c3e1a8f1cecf979bc619d2174a99f5f
sanghavivraj <102163202+sanghavivraj@users.noreply.github.com>
2025-12-07 18:51:46 +0530

    Create PaymentDTO file

---
+++ src/main/java/com/example/Smart_Parking/DTO/PaymentDTO.java
@@ -0,0 +1,26 @@
+package com.example.Smart_Parking.DTO;
+
+public class PaymentDTO {
+    private Long userId;
+    private Long reserveId;
+    private Double amount;
+    private String method; 
+    private String upiTransactionId;
+
+    public PaymentDTO() {}
+
+    public Long getUserId() { return userId; }
+    public void setUserId(Long userId) { this.userId = userId; }
+
+    public Long getReserveId() { return reserveId; }
+    public void setReserveId(Long reserveId) { this.reserveId = reserveId; }
+
+    public Double getAmount() { return amount; }
+    public void setAmount(Double amount) { this.amount = amount; }
+
+    public String getMethod() { return method; }
+    public void setMethod(String method) { this.method = method; }
+
+    public String getUpiTransactionId() { return upiTransactionId; }
+    public void setUpiTransactionId(String upiTransactionId) { this.upiTransactionId = upiTransactionId; }
+}
